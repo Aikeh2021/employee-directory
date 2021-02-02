5f06fe4a5082ef89765fc81cc2673edc4eff28b3
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-02 15:04:27 -0500

    Created a header component and gave it some content. Also added a divider div to the app.js to put a line between the start of the table and the the heading on the employee directory. Imported the header into the app.js.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Table from './components/Table/Table'
+import Table from './components/Table/Table';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className="container">
+      <div className="row">
+        <Header />
+      </div>
+      <div className="divider"></div>
       <div className="row">
         <div className="col s12">
         <Table />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+          <h1>GT Computer Science Staff Directory:</h1>  
+        </div>
+    );
+};
+
+export default Header;
