bf958faf9dfe22fd2dba23c72cef66f86695861f
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-02 13:53:46 -0500

    Created a table component in the components folder. Use the react shortcuts to scaffold out a stateless component. Imported a sample table form Materialize and gave it the className of striped. Imported the table component into the app.js file just to make sure the styling is being applied like I wanted.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Table from './components/Table/Table'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Table />
+    </>
   );
 };
 
