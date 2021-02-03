c73f9917b83c96049b1f49ac28054d599e5d2afa
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-03 11:13:45 -0500

    So far the sortByName funciton is working. Now to get the sorted users to appear on the screen.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,51 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Table = () => {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://randomuser.me/api/?results=30").then(response => {
-            console.log(response.data);
-            setUsers(response.data.results);
-        })
-    }, []);
+  useEffect(() => {
+    axios.get("https://randomuser.me/api/?results=30").then((response) => {
+      console.log(response.data);
+      setUsers(response.data.results);
+    });
+  }, []);
 
-    const sortByName = () => {
-        console.log("I was clicked!")
-        const sortedUsers = users.sort((a,b) => b.name.first - a.name.first)
-        console.log(sortedUsers);
+  const sortByName = () => {
+    console.log("I was clicked!");
+    const sortedUsers = users.sort((a, b) => {
+      let nameA = a.name.first;
+      let nameB = b.name.first;
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    });
+    console.log(sortedUsers);
+  };
 
-    }
-    return (
-        <table className="striped">
-        <thead>
-          <tr>
-              <th className="s2">ID</th>
-              <th className="s2">Photo</th>
-              <th className="s2" onClick={sortByName}>Name </th>
-              <th className="s2">Email</th>
-              <th className="s2">Phone Number</th>
-              <th className="s2">Date of Birth (DOB)</th>
+  return (
+    <table className="striped">
+      <thead>
+        <tr>
+          <th className="s2">ID</th>
+          <th className="s2">Photo</th>
+          <th className="s2" onClick={sortByName}>
+            Name{" "}
+          </th>
+          <th className="s2">Email</th>
+          <th className="s2">Phone Number</th>
+          <th className="s2">Date of Birth (DOB)</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id.value}>
+            <th className="s2">{user.id.value}</th>
+            <td className="s2">
+              <img src={user.picture.thumbnail} alt="User's thumbnail" />
+            </td>
+            <td className="s2">
+              {user.name.first} {user.name.last}
+            </td>
+            <td className="s2">{user.email}</td>
+            <td className="s2">{user.phone}</td>
+            <td className="s2">{user.dob.date}</td>
           </tr>
-        </thead>
-
-        <tbody>
-          {users.map((user) => (
-              <tr key={user.id.value}>
-              <th className="s2">{user.id.value}</th>
-              <td className="s2"><img src={user.picture.thumbnail} alt="User's thumbnail"/></td>
-              <td className="s2">{user.name.first} {user.name.last}</td>
-              <td className="s2">{user.email}</td>
-              <td className="s2">{user.phone}</td>
-              <td className="s2">{user.dob.date}</td>
-            </tr>
-          ))}
-
-        </tbody>
-      </table>
-            
-    );
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
