9f1d962dd76e65ceab003f3c4d5cc394cb57c2ba
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-02 23:19:39 -0500

    Basic server set up is complete.

--- server.js
+++ server.js
@@ -2,5 +2,19 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || PORT;
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+    res.json(
+        {
+            isworking: true
+        }
+    )
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
+});
